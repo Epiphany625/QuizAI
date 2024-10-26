616625502d36c99d6d79b74d8c350a156a2f0793
Xinyang Xu <epiphany0625@outlook.com>
2024-10-26 14:42:47 -0700

    initialize react-router-dom

--- QuizAI_frontend/src/App.jsx
+++ QuizAI_frontend/src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react'
 import Landing from './components/landing/Landing.jsx'
+import Quiz from './components/quiz/Quiz.jsx'
+import Summary from './components/summary/Summary.jsx'
+import Chatbot from './components/chatbot/Chatbot.jsx'
+import {Routes, Route} from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
-    <>
-    <Landing />
-    </>
+    <Routes>
+      <Route path='/' element={<Landing />} />
+      <Route path='/summary' element={<Summary />} />
+      <Route path='/quiz' element={<Quiz/>} />
+      <Route path='/chatbot' element={<Chatbot />} />
+    </Routes>
   )
 }
 
