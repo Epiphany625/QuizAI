2147c31f1d0d951a993aadaab44ae548b336599e
Minyu Huang <mnyhu201@gmail.com>
2024-10-27 21:39:19 -0700

    added login error message

--- QuizAI_frontend/src/components/landing/Landing.jsx
+++ QuizAI_frontend/src/components/landing/Landing.jsx
@@ -17,17 +17,21 @@ const Landing = () => {
     // manage react router
     const navigation = useNavigate();
 
+    // track if user entered invalid login credentials, or other error messages
+    const [errorMessage, setErrorMessage] = useState(''); // Track login error message
 
     // functions
     // Display selection handlers
     const chooseSignUp = () => {
         setSignUpDisplay('block');
         setLoginDisplay('none');
+        setErrorMessage(''); // Clear error when switching forms
     };
 
     const chooseLogin = () => {
         setSignUpDisplay('none');
         setLoginDisplay('block');
+        setErrorMessage(''); // Clear error when switching forms
     };
 
     // Validation function for email and password length
@@ -41,7 +45,7 @@ const Landing = () => {
         
         // Form validation
         if (!lengthValidation()) {
-            loginButton.current.style.backgroundColor = "red";
+            setErrorMessage("Invalid length")
             return; 
         }
         const data = { "email": email, "password": password };
@@ -60,10 +64,12 @@ const Landing = () => {
                 console.log(response);
                 navigation('/summary');
             } else {
+                setErrorMessage('Invalid login credentials');
                 console.log(response.statusText);
             }
         } catch (err) {
             console.log(err);
+            setErrorMessage('Something went wrong. Please try again later.');
         }
     };
 
@@ -72,9 +78,12 @@ const Landing = () => {
         event.preventDefault();
         
         // Form validation
-        if (!lengthValidation() || confirmPassword !== password) {
-            signUpButton.current.style.backgroundColor = "red";
+        if (!lengthValidation()) {
+            setErrorMessage("Invalid length");
             return; 
+        } else if(confirmPassword != password){
+            setErrorMessage("Passwords don't match");
+            return;
         }
 
         const data = { "email": email, "password": password };
@@ -107,90 +116,122 @@ const Landing = () => {
                 <h4>Empower your learning journey with <i>Gemini AI</i></h4>
             </div>
             
-            <div className='forms'>
-                <div className='form-selection'>
+            {/* <div className='forms'> */}
+                {/* <div className='form-selection'>
                     <button onClick={chooseSignUp}>Sign Up</button>
                     <button onClick={chooseLogin}>Log In</button>
-                </div>
+                </div> */}
                 
-                <div id="signup-login-form">
-                    <form 
-                        className="signup-form" 
-                        style={{ display: signupDisplay }} 
-                        onSubmit={handleSignUp}
-                    >
-                        <div className="field">
-                            <label htmlFor="signup-email">Email: </label>
-                            <input 
-                                id="signup-email" 
-                                className="user-input" 
-                                type="email" 
-                                value={email} 
-                                onChange={(e) => setEmail(e.target.value)} 
-                            />
-                        </div>
-                        
-                        <div className="field">
-                            <label htmlFor="signup-password">Password: </label>
-                            <input 
-                                id="signup-password" 
-                                className="user-input" 
-                                type="password" 
-                                value={password} 
-                                onChange={(e) => setPassword(e.target.value)} 
-                            />
-                        </div>
-                        
-                        <div className="field">
-                            <label htmlFor="signup-confirmPassword">Confirm Password: </label>
-                            <input 
-                                id="signup-confirmPassword" 
-                                className="user-input" 
-                                type="password" 
-                                value={confirmPassword} 
-                                onChange={(e) => setConfirmPassword(e.target.value)} 
-                            />
-                        </div>
-                        
-                        <div style={{ paddingTop: "10px" }}>
-                            <button ref={signUpButton}>Sign Up</button>
-                        </div>
-                    </form>
+                <div className='forms'>
+                    <div className='form-selection'>
+                        <button 
+                            onClick={chooseSignUp} 
+                            className={signupDisplay === "block" ? 'active-button' : ''}
+                        >
+                            Sign Up
+                        </button>
+                        <button 
+                            onClick={chooseLogin} 
+                            className={loginDisplay === "block" ? 'active-button' : ''}
+                        >
+                            Log In
+                        </button>
+                    </div>
+                    
+                    <div id="signup-login-form">
+                        <form 
+                            className="signup-form" 
+                            style={{ display: signupDisplay }} 
+                            onSubmit={handleSignUp}
+                        >
+                            <div className="field">
+                                <label htmlFor="signup-email">Email: </label>
+                                <input 
+                                    id="signup-email" 
+                                    className="user-input" 
+                                    type="email" 
+                                    value={email} 
+                                    onChange={(e) => setEmail(e.target.value)} 
+                                />
+                            </div>
+                            
+                            <div className="field">
+                                <label htmlFor="signup-password">Password: </label>
+                                <input 
+                                    id="signup-password" 
+                                    className="user-input" 
+                                    type="password" 
+                                    value={password} 
+                                    onChange={(e) => setPassword(e.target.value)} 
+                                />
+                            </div>
+                            
+                            <div className="field">
+                                <label htmlFor="signup-confirmPassword">Confirm Password: </label>
+                                <input 
+                                    id="signup-confirmPassword" 
+                                    className="user-input" 
+                                    type="password" 
+                                    value={confirmPassword} 
+                                    onChange={(e) => setConfirmPassword(e.target.value)} 
+                                />
+                            </div>
+                            
+                            {/* Error message */}
+                            {errorMessage && (
+                                <div className="error-message">
+                                    {errorMessage}
+                                </div>
+                            )}
 
-                    <form 
-                        className="login-form" 
-                        style={{ display: loginDisplay }} 
-                        onSubmit={handleLogin}
-                    >
-                        <div className="field">
-                            <label htmlFor="login-email">Email: </label>
-                            <input 
-                                id="login-email" 
-                                type="email" 
-                                className="user-input" 
-                                value={email} 
-                                onChange={(e) => setEmail(e.target.value)} 
-                            />
-                        </div>
-                        
-                        <div className="field">
-                            <label htmlFor="login-password">Password: </label>
-                            <input 
-                                id="login-password" 
-                                type="password" 
-                                className="user-input" 
-                                value={password} 
-                                onChange={(e) => setPassword(e.target.value)} 
-                            />
-                        </div>
-                        
-                        <div style={{ paddingTop: "10px" }}>
-                            <button ref={loginButton}>Log In</button>
-                        </div>
-                    </form>
+                            <div style={{ paddingTop: "10px" }}>
+                                <button ref={signUpButton}>Sign Up</button>
+                            </div>
+                            
+                        </form>
+
+                        <form 
+                            className="login-form" 
+                            style={{ display: loginDisplay }} 
+                            onSubmit={handleLogin}
+                        >
+                            <div className="field">
+                                <label htmlFor="login-email">Email: </label>
+                                <input 
+                                    id="login-email" 
+                                    type="email" 
+                                    className="user-input" 
+                                    value={email} 
+                                    onChange={(e) => setEmail(e.target.value)} 
+                                />
+                            </div>
+                            
+                            <div className="field">
+                                <label htmlFor="login-password">Password: </label>
+                                <input 
+                                    id="login-password" 
+                                    type="password" 
+                                    className="user-input" 
+                                    value={password} 
+                                    onChange={(e) => setPassword(e.target.value)} 
+                                />
+                            </div>
+
+                            {/* Error Message */}
+                            {errorMessage && (
+                                <div className="error-message">
+                                    {errorMessage}
+                                </div>
+                            )}
+                            
+                            <div style={{ paddingTop: "10px" }}>
+                                <button ref={loginButton}>Log In</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        // </div>
     );
 };
 
