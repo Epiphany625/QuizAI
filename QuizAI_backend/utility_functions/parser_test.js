5e817b0e4e645b989aea9c4dc3c0119b94f8cee4
Minyu Huang <mnyhu201@gmail.com>
2024-11-16 16:27:47 -0800

    added support for more question types (short answer, mcq, t/f) and updated json_parser

--- QuizAI_backend/utility_functions/parser_test.js
+++ QuizAI_backend/utility_functions/parser_test.js
@@ -2,18 +2,21 @@ import fs from 'fs';
 import parseQuizToJson from './json_parser.js';
 
 
-let quizText = "# Which organ is responsible for pumping blood throughout the body? # A) Brain B) Heart C) Kidney D) Liver --- B";
-quizText += "# what's the power house of the cell? # A) cell wall B) cellulose string C) mitochondria D) Liver --- C";
+let mcqText = `# Which organ is responsible for pumping blood throughout the body? # A) Brain B) Heart C) Kidney D) Liver --- B
+# what's the power house of the cell? # A) cell wall B) cellulose string C) mitochondria D) Liver --- C`;
+let shortAnswerText = `# Which organ is responsible for pumping blood throughout the body? # Brain
+# what's the power house of the cell? # mitochondria`;
 
-const quizJson = parseQuizToJson(quizText);
+
+const quizJson = parseQuizToJson(shortAnswerText, "short answer");
 const jsonString = JSON.stringify(quizJson, null, 2);
 // print out the json string
 console.log(jsonString);
 
-fs.writeFile('quiz.json', jsonString, (err) => {
-  if (err) {
-    console.error('Error writing to file', err);
-  } else {
-    console.log('File written successfully');
-  }
-});
+// fs.writeFile('quiz.json', jsonString, (err) => {
+//   if (err) {
+//     console.error('Error writing to file', err);
+//   } else {
+//     console.log('File written successfully');
+//   }
+// });
