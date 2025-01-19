3ee09332b7b287f8f3f349eda64d81ab10166dd5
Xinyang Xu <epiphany0625@outlook.com>
2025-01-18 18:52:24 -0800

    finished frontend and initialized backend for quiz saving

--- QuizAI_backend/controllers/quizController.js
+++ QuizAI_backend/controllers/quizController.js
@@ -1,4 +1,5 @@
 import { generateQuiz } from '../utility_functions/Gemini_API.js';
+import User from '../models/user.js';
 
 export const postQuiz = async (req, res) => {
     console.log("post quiz...");
@@ -7,6 +8,26 @@ export const postQuiz = async (req, res) => {
     const type = req.body.questionType ?? 'multiple-choice';
     const example = req.body.exampleQuestion ?? null;
 
+    // generate the quiz
     const quiz = await generateQuiz(req.body.content, num, type, example);
+
+    // save the quiz to the database, if the user has selected a course
+    if(req.body.course){
+        const courseName = req.body.course; 
+        const user = await User.findOne({email: req.body.email});
+
+        for(let i = 0; i < user.courses.length; i++){
+            if(user.courses[i].name === courseName){
+                const currCourse = user.courses[i];
+                const newQuiz = new Quiz({url: req.body.url, questions: []});
+                for(let j = 0; j < quiz.length; j++) {
+                    const currQuestion = quiz[j];
+                    const newQuestion = new Question({question, choices, correctAnswer});
+                    newQuiz.questions.push(newQuestion);
+                }
+                currCourse.quizzes.push(newQuiz);
+            }
+        }
+    }
     res.status(200).json({quiz: quiz});
 }
